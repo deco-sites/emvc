8ea87ba4fe80128145f14411defc17b8fbc9f803
thallesyam <thallesyam@gmail.com>
2023-05-14 15:42:35 -0300

    feat: add font display

--- components/GlobalTags.tsx
+++ components/GlobalTags.tsx
@@ -13,36 +13,42 @@ function GlobalTags() {
             src: url(${asset("/fonts/Inter-Medium.ttf")}) format('ttf');
             font-weight: 500;
             font-style: normal;
+            font-display: swap;
           }
           @font-face {
             font-family: 'Inter';
             src: url(${asset("/fonts/Inter-SemiBold.woff")}) format('woff');
             font-weight: 600;
             font-style: normal;
+            font-display: swap;
           }
           @font-face {
             font-family: 'Inter';
             src: url(${asset("/fonts/Inter-Regular.woff")}) format('woff');
             font-weight: 400;
             font-style: normal;
+            font-display: swap;
           }
           @font-face {
             font-family: 'Sora';
             src: url(${asset("/fonts/Sora-Regular.woff2")}) format('woff2');
             font-weight: 400;
             font-style: normal;
+            font-display: swap;
           }
           @font-face {
             font-family: 'Sora';
             src: url(${asset("/fonts/Sora-Bold.woff2")}) format('woff2');
             font-weight: 700;
             font-style: normal;
+            font-display: swap;
           }
           @font-face {
             font-family: 'Sora';
             src: url(${asset("/fonts/Sora-ExtraBold.woff2")}) format('woff2');
             font-weight: 800;
             font-style: normal;
+            font-display: swap;
           }
       `,
         }}
