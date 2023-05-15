7e0de6cf2692c61ace4036da297adffc4e6ac6de
thallesyam <thallesyam@gmail.com>
2023-05-15 11:09:13 -0300

    style: add screen

--- components/ui/Banner.tsx
+++ components/ui/Banner.tsx
@@ -58,7 +58,7 @@ function BannerUI(
           media="(min-width: 767px)"
         />
         <img
-          class={`max-w-none object-fill`}
+          class={`w-screen object-fill`}
           src={image.desktop}
           alt={image.alt}
         />
